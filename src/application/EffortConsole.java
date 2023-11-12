02fb45ac852ff2a3510d830b90eb426eb7a8dafa
bailslrnd <149340611+bailslrnd@users.noreply.github.com>
2023-11-12 16:51:41 -0700

    Update EffortConsole.java

--- src/application/EffortConsole.java
+++ src/application/EffortConsole.java
@@ -165,6 +165,32 @@ public class EffortConsole implements Initializable{
 		loadDeliverables();
 		loadCards();
 	}
-    
+  	ArrayList<String> words = new ArrayList<> (
+    	     Arrays.asList("EffortLoggerV1", "VerificationSystem", "LoginSystem", "TaskEditor")
+    	);
+    	
+    	@FXML
+   	private TextField searchBar;
+    	
+   	@FXML
+   	private ListView<String> listView;
+   	
+   	@FXML
+   	protected void search(ActionEvent event) {
+    	listView.getItems().clear();
+    	listView.getItems().addAll(searchList(searchBar.getText(),words));
+   	}
+   	
+   	/*public void initialize(URL url, ResourceBundle resourceBundle) {
+    	listView.getItems().addAll(words);
+        }*/
+   	
+   	private List<String> searchList(String searchWords, List<String> listOfStrings) {
+   		List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
+   		return listOfStrings.stream().filter(input -> { //input = test
+    		return searchWordsArray.stream().allMatch(word ->  //word = t
+    		input.toLowerCase().contains(word.toLowerCase()));
+    		}).collect(Collectors.toList());
+   	}
 
 }
